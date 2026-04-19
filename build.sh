ebcfd635e6b2fd92be7418ee65307d9449497b6d
Aman Chaudhary <amanchaudhary4510@gmail.com>
2026-04-19 18:36:10 +0530

    fix flutter version issue

--- build.sh
+++ build.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# Flutter install
-git clone https://github.com/flutter/flutter.git --depth 1
+# Clone Flutter stable version
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
+
+# Set path
 export PATH="$PATH:`pwd`/flutter/bin"
+
+# Check version (important fix)
+flutter --version
 
 # Enable web
 flutter config --enable-web
 
-# Get dependencies
+# Clean & get packages
+flutter clean
 flutter pub get
 
 # Build web
